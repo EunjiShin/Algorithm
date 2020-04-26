79c2ab5cbe92560ec705d6d245897a1796925a1a
EunjiShin <simsimhae_98@naver.com>
2020-04-27 00:07:32 +0900

    modify

--- problem7785.java
+++ problem7785.java
@@ -2,30 +2,32 @@ import java.io.*;
 import java.util.*;
 
 public class problem7785 {
+
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         String name;
         String home;
 
-        ArrayList<String> list = new ArrayList<String>();
+        Set<String> set = new TreeSet<>();
 
-        for(int i=0; i<n; i++){
-            name = sc.next();
-            home = sc.next();
-            if(home.equals("enter")){
-                list.add(name);
-            }
-            if(home.equals("leave")){
-                list.remove(name);
-            }
-        }
+       for(int i=0; i<n; i++){
+           name = sc.next();
+           home = sc.next();
+           if(home.equals("enter")){
+               set.add(name);
+           }
+           else{
+               set.remove(name);
+           }
+       }
 
-        list.sort(null);
 
-        for(int i=0; i<list.size(); i++) {
-            System.out.println(list.get(i));
-        }
+       for(Iterator<String> itr = ((TreeSet<String>) set).descendingIterator(); itr.hasNext();){
+           System.out.println(itr.next());
+       }
+
 
     }
 }
+
