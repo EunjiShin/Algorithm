66b59620c173fad613c696af16055473661c4b6c
EunjiShin <simsimhae_98@naver.com>
2020-05-13 16:23:24 +0900

    rewrite 11652

--- problem11652.java
+++ problem11652.java
@@ -1,41 +1,63 @@
 import java.io.*;
 import java.util.*;
-import static java.util.Arrays.sort;
+import java.math.*;
 
 public class problem11652 {
+
+    static long array[];
     public static void main(String [] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        long []array = new long [n];
-        long max=1;
+        array = new long [n];
+        int max=0;
         int count=1;
-        long num=array[0];
+        long num=0;
 
         for(int i=0; i<n; i++){
             array[i] = sc.nextLong();
         }
 
+        if(n==1){
+            System.out.println(array[0]);
+            return;
+        }
+
         Arrays.sort(array);
 
         for(int i=0; i<n-1; i++){
-            if(array[i]==array[i+1]){
-                count++;
-                if(count > max){
+            if(i == n-2){
+                if(array[i] == array[i+1]){
+                    count++;
+                }
+                if(max < count){
                     max = count;
                     num = array[i];
+                    count = 1;
                 }
+                else if(max == count){
+                    //num =  num - array[i]>0 ? num: array[i];
+                    num = Math.min(num, array[i]);
+                }
+            }
+            else if(array[i]==array[i+1]){
+                count++;
             }
             else{
-                count=1;
-            }
-
-           /* if(count==max){
-                if(array[i]<0 && array[i-1]<0){
+                if(max < count){
+                    max = count;
                     num = array[i];
+                    count = 1;
+                }
+                else if(max == count){
+                    //num =  num - array[i]>0 ? num: array[i];
+                    num = Math.min(num, array[i]);
+                    max = count;
+                    count = 1;
+                }
+                else {
+                    count = 1;
                 }
             }
-            */
-
         }
 
         System.out.println(num);
